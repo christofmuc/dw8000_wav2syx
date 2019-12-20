25dc5648b78d9079e38533bf3016a93d4c8d9c50
cruc <christof.ruch@gmx.de>
2019-12-20 21:35:21 +0100

    Adding copyright headers.

--- reverseEngineerDW8000.py
+++ reverseEngineerDW8000.py
@@ -1,3 +1,9 @@
+#
+#  Copyright (c) 2019 Christof Ruch. All rights reserved.
+#
+#  Dual licensed: Distributed under Affero GPL license by default, an MIT license is available for purchase
+#
+
 from math import log2, ceil, floor
 
 import mido
