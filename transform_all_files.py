934e7dfcbef99137ac68fc0779642eee171179c3
cruc <christof.ruch@gmx.de>
2019-12-21 16:18:20 +0100

    Repair the "test all files I have" function in order to see where I am with the current code.

--- transform_all_files.py
+++ transform_all_files.py
@@ -7,9 +7,18 @@
 import os
 
 from dw8000_wav2syx.dw8000_wav2bin import transform_wav_to_bytes
+from dw8000_wav2syx.dw8000_bin2syx import remap_tape_data_to_syx
 
-for root, dirs, files in os.walk(r"g:\christof\music\dw8000\patches"):
+for root, dirs, files in os.walk(r"test_data"):
     for file in files:
         filename, file_extension = os.path.splitext(file)
         if file_extension.lower() == ".wav":
-            transform_wav_to_bytes(os.path.join(root, file), os.path.join(root, filename + ".auto.bin"))
+            output_filename = os.path.join(root, filename + ".auto.bin")
+            with open(output_filename, "w+b") as output_file:
+                worked = transform_wav_to_bytes(os.path.join(root, file), output_file)
+                if worked:
+                    # Rewind bin file so the next function will read from the start again
+                    output_file.seek(0)
+                    syx_filename = os.path.join(root, filename + ".auto.syx")
+                    remap_tape_data_to_syx(tapefile=output_file, syxfile=syx_filename)
+
